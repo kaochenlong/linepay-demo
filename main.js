c56d23cc82388ef7a81637eefa3ac737cd32826f
Eddie Kao <eddie@5xcampus.com>
2025-06-05 20:08:36 +0800

    calc signature

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import express from "express"
+import crypto from "crypto"
 import "dotenv/config"
+
+const host = "https://sandbox-api-pay.line.me/v3"
+
+function signKey(clientKey, msg) {
+  const encoder = new TextEncoder()
+  return crypto
+    .createHmac("sha256", encoder.encode(clientKey))
+    .update(encoder.encode(msg))
+    .digest("base64")
+}
 
 const app = express()
 app.set("views", "./templates")
@@ -16,6 +27,46 @@ app.get("/cart/order", (req, res) => {
 
 app.post("/payment/linepay", (req, res) => {
   // 跳轉 linepay 頁面
+  const { LINEPAY_CHANNEL_ID, LINEPAY_CHANNEL_SECRET_KEY } = process.env
+  const apiPath = "/payments/request"
+  const nonce = crypto.randomUUID()
+  const data = {
+    amount: 100,
+    currency: "TWD",
+    orderId: orderGenerator(),
+    packages: [
+      {
+        id: "1",
+        amount: 100,
+        products: [
+          {
+            id: "PEN-B-001",
+            name: "Pen Brown",
+            imageUrl: "https://pay-store.example.com/images/pen_brown.jpg",
+            quantity: 2,
+            price: 50,
+          },
+        ],
+      },
+    ],
+    redirectUrls: {
+      confirmUrl: "http://localhost:3888/order/confirm",
+      cancelUrl: "http://localhost:3888/order/cancel",
+    },
+  }
+  const msg = `${LINEPAY_CHANNEL_SECRET_KEY}${apiPath}${JSON.stringify(
+    data
+  )}${nonce}`
+
+  const sig = signKey(LINEPAY_CHANNEL_SECRET_KEY, msg)
+  const headers = {
+    "Content-Type": "application/json",
+    "X-LINE-ChannelId": LINEPAY_CHANNEL_ID,
+    "X-LINE-Authorization": sig,
+    "X-LINE-Authorization-Nonce": nonce,
+  }
+
+  console.log(headers)
 
   res.redirect("/payment/ok")
 })
